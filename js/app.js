1a9fe2c17d733aa02ee86fce84b3f6c528edd883
sofiduron <113409102+sofiduron@users.noreply.github.com>
2022-12-07 22:00:20 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,102 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'AppBase',
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/profile/',
+    	url: 'profile.html',
+    	name: 'profile',
+  		},
+		{
+		path: '/brands/',
+    	url: 'brands.html',
+    	name: 'brands',
+  		},
+		{
+		path: '/products/',
+    	url: 'products.html',
+    	name: 'products',
+  		},
+		{
+		path: '/info-product/',
+    	url: 'info-product.html',
+    	name: 'info-product',
+  		},
+		{
+		path: '/cart/',
+    	url: 'cart.html',
+    	name: 'cart',
+  		},
+	],
+	dialog: {
+		title: 'AppBase',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#login');
+		
+		app.dialog.alert('!Welcome to AppBase !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnCheckout', function (e) {
+	e.preventDefault();
+
+	
+	var notification = app.notification.create({
+      icon: '<i class="material-icons">check</i>',
+      title: 'Order',
+      titleRightText: '',
+      subtitle: '',
+      text: "Your order has been received.",
+      closeTimeout: 3000,
+    });
+    notification.open();
+	
+});
+
+
+
+
+
